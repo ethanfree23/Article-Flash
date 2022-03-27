10ed6c7a9d0668d0cb059360d3f0c799f8f2178d
Ethan Freeman <ethanfree23@gmail.com>
2022-03-27 11:58:08 -0500

    links on flashcard page

--- client/src/Flashcards.js
+++ client/src/Flashcards.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import Header from './Header.js';
 import FlashcardList from './FlashcardList.js';
+import CardList from './features/CardList.jsx'
 
 
 function Flashcards({ flashcards }) {
     return (
         <>
             <Header />
+            <div>
+                <Link to="/add-card"> add a card</Link>
+            </div>
             <FlashcardList flashcards={flashcards} />
+            <CardList />
         </>
     )
 }
